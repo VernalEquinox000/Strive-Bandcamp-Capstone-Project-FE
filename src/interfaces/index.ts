a0ed4fb9c0969128281b31a69400869254b73e2f
VernalEquinox <pieroapruzzese@gmail.com>
2021-03-25 21:37:26 +0100

    setting user interface + apiUser

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,15 @@
 export interface User {
     email: string,
-    password: string    
+    password: string,
+    //password must be of 3 chars
+    username?:string,
+    artistName?:string, 
+    labelName?: string,
+    location?: string,
+    url?: string,
+    //role?: enum,
+    //refreshTokens[]?: string,
+    googleId?: string
+    picture?: string
 }
 
