19742f0ba1a0b48a26011e59de86ed697da7ae60
VernalEquinox <pieroapruzzese@gmail.com>
2021-04-05 22:07:42 +0200

    Create AlbumCard.jsx

---
+++ src/components/AlbumCard.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+
+export default function AlbumCard() {
+  /* const [loading, setLoading] = useState(false);
+    const [albumId, setAlbumId] = useState("");
+    const [albumTitle, setAlbumTitle] = useState("");
+    const [albumCover, setAlbumCover] = useState(
+        "https://via.placeholder.com/100x100.png"
+    ); */
+  return (
+    <div>
+      <Col>
+        <Card id={albumId} style={{ width: "12rem" }}>
+          <Card.Img variant="top" src={cover} />
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Button variant="primary">{audioFile}</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </div>
+  );
+}
