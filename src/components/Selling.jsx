deb3f5a46f5dc44545561188a355956a6d4f9230
VernalEquinox <pieroapruzzese@gmail.com>
2021-04-08 23:40:56 +0200

    fetch user / artists set

--- src/components/Selling.jsx
+++ src/components/Selling.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { getAllUsers } from "../api/userApi";
@@ -7,102 +7,46 @@ export default function Selling() {
   const [albumId, setAlbumId] = useState("");
   const [albumTitle, setAlbumTitle] = useState("");
   const [albumCover, setAlbumCover] = useState("https://via.placeholder.com/100x100.png"); */
-  const [artists, setArtists] = useState([]);
+  const [artists, setArtists] = useState(null);
 
   const fetchUsers = async () => {
     const response = await getAllUsers();
     /* if (response.statusText === "OK") { */
     const data = await response.data;
     console.log(data);
-    console.log(data.role);
-    setArtists(data);
+    const filterArtists = data.filter((d) => d.role === "artist");
+    setArtists(filterArtists);
     console.log(artists);
     /* } else {
       alert("something went wrong");
     } */
   };
 
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div>
       <Container className="d-flex flex-column justify-content-center align-content-center">
         <Row>
-          <Link
-            to={`/artist/606b7e9a1c2e4735a3988a1d`}
-            activeClassName="active"
-          >
-            <Col xl={4} className="position-relative">
-              <Card style={{ width: "12rem" }}>
-                <Card.Img
-                  variant="top"
-                  src="https://res.cloudinary.com/vernalequinox000/image/upload/v1617691012/profile/s626gewlanehxxhxgkb1.jpg"
-                />
-                <Card.Body>
-                  <Card.Title>The Telescopes</Card.Title>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Link>
-
-          <Link
-            to={`/artist/6063a38923feb85e4d745853`}
-            activeClassName="active"
-          >
-            <Col xl={4} className="position-relative">
-              <Card style={{ width: "12rem" }}>
-                <Card.Img
-                  variant="top"
-                  src="https://res.cloudinary.com/vernalequinox000/image/upload/v1617743258/profile/tjjvnygk2twwzhgntesf.jpg"
-                />
-                <Card.Body>
-                  <Card.Title>Acid Mothers Temple</Card.Title>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Link>
-          <Link>
-            <Col xl={4} className="position-relative">
-              <Card style={{ width: "12rem" }}>
-                <Card.Img
-                  variant="top"
-                  src="https://via.placeholder.com/100x100.png"
-                />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Link>
-          <Link>
-            <Col xl={4} className="position-relative">
-              <Card style={{ width: "12rem" }}>
-                <Card.Img
-                  variant="top"
-                  src="https://via.placeholder.com/100x100.png"
-                />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Link>
-          <Link>
-            <Col xl={4} className="position-relative">
-              <Card style={{ width: "12rem" }}>
-                <Card.Img
-                  variant="top"
-                  src="https://via.placeholder.com/100x100.png"
-                />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Link>
+          {artists &&
+            artists.map((artist) => (
+              <Link
+                to={{ pathname: `/artist/${artist._id}` }}
+                activeClassName="active"
+              >
+                <Col xl={4} className="position-relative">
+                  <Card style={{ width: "12rem" }}>
+                    <Card.Img variant="top" src={artist.profilePic} />
+                    <Card.Body>
+                      <Card.Title>{artist.artistName}</Card.Title>
+                      <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Link>
+            ))}
         </Row>
       </Container>
     </div>
