f6b3229cb16c61b258cb0a2d05dbbfe1addbc2ad
VernalEquinox <pieroapruzzese@gmail.com>
2021-04-15 17:28:52 +0200

    tryin user with redux,add store + reducer

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import userReducer from "../reducers/user";
+import thunk from "redux-thunk";
+
+const composedEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const initialState = {
+  user: null,
+};
+
+const allReducers = combineReducers({
+  user: userReducer,
+});
+
+export default function configureStore() {
+  return createStore(
+    allReducers,
+    initialState,
+    composedEnhancer(applyMiddleware(thunk))
+  );
+}
