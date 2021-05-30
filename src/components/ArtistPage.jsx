d3a86fc75536a258107a01f2f170800ddaa95f06
Pierdomenico Apruzzese <pieroapruzzese@gmail.com>
2021-05-30 22:14:53 +0200

    more work on artist page style

--- src/components/ArtistPage.jsx
+++ src/components/ArtistPage.jsx
@@ -43,107 +43,94 @@ export default function ArtistPage() {
 
   //className = "row-cols-1 row-cols-sm-2 row-cols-lg-4 mb-4 text-left";
   return (
-    <div>
+    <Container
+      className="d-flex flex-column justify-content-center align-content-center"
+      style={{
+        marginRight: "auto",
+        marginLeft: "auto",
+        flexGrow: "1",
+      }}
+    >
       {loader ? (
-        <Container className="mb-5">
-          <Row>
-            <Col sm={{ span: 10, offset: 1 }}>
-              <Spinner animation="grow" variant="dark" />
-            </Col>
-          </Row>
-        </Container>
+        <Row>
+          <Col sm={{ span: 10, offset: 1 }}>
+            <Spinner animation="grow" variant="dark" />
+          </Col>
+        </Row>
       ) : (
         <>
           {error && <Alert variant="danger">{error}</Alert>}
-          {artist && (
-            <Container
-              className="d-flex flex-column justify-content-center align-content-center"
-              style={{
-                marginRight: "auto",
-                marginLeft: "auto",
-                flexGrow: "1",
-              }}
-            >
-              <ArtistBar key={artist._id} header={artist.headerPic} />
-            </Container>
-          )}
+          {artist && <ArtistBar key={artist._id} header={artist.headerPic} />}
         </>
       )}
-      <Container
-        className="d-flex flex-column justify-content-center align-content-center"
+
+      <Row
+        className="row-cols-1 row-cols-sm-2 row-cols-md-5 row-cols-lg-5  artist-row mt-5"
         style={{
-          marginRight: "auto",
-          marginLeft: "auto",
-          flexGrow: "1",
+          width: "100%",
         }}
       >
-        <Row
-          className="row-cols-6  artist-row mt-5"
-          style={{
-            width: "100%",
-          }}
-        >
+        {loader ? (
+          [0, 1, 2, 3, 4, 5].map((item) => (
+            <Col className="col text-center mb-2 mb-lg-0 px-1" key={item}>
+              <Spinner animation="grow" variant="dark" />
+            </Col>
+          ))
+        ) : (
+          <>
+            {error && <Alert variant="danger">{error}</Alert>}
+            {artist &&
+              artist.albums.map((album) => (
+                <Col className="col text-center mb-2">
+                  <Link
+                    to={{
+                      pathname: `/artist/${artist._id}/album/${album._id}`,
+                    }}
+                    artist={artist}
+                  >
+                    <div class=" position-relative" key={album._id}>
+                      <img
+                        class="img-fluid rounded"
+                        src={album.cover}
+                        style={{ objectFit: "contain" }}
+                      />
+                      <h6 className="artist-album-title">{album.title}</h6>
+                    </div>
+                  </Link>
+                </Col>
+              ))}
+          </>
+        )}
+
+        <Col className="col text-center mb-2 mb-lg-0 px-1">
           {loader ? (
-            [0, 1, 2, 3, 4, 5].map((item) => (
-              <Col className="col text-center mb-2 mb-lg-0 px-1" key={item}>
+            <Row>
+              <Col>
                 <Spinner animation="grow" variant="dark" />
               </Col>
-            ))
+            </Row>
           ) : (
             <>
               {error && <Alert variant="danger">{error}</Alert>}
-              {artist &&
-                artist.albums.map((album) => (
-                  <Col className="col text-center mb-2 mb-lg-0 px-1">
-                    <Link
-                      to={{
-                        pathname: `/artist/${artist._id}/album/${album._id}`,
-                      }}
-                      artist={artist}
-                    >
-                      <div class=" position-relative" key={album._id}>
-                        <img
-                          class="img-fluid rounded"
-                          src={album.cover}
-                          style={{ height: "150px", objectFit: "contain" }}
-                        />
-                        <h6 className="artist-album-title">{album.title}</h6>
-                      </div>
-                    </Link>
-                  </Col>
-                ))}
+              {artist && (
+                <Col
+                  className="ml-auto"
+                  xs={{ span: 2, order: 1 }}
+                  md={{ span: 3, order: 12 }}
+                  lg={{ span: 2, order: 12 }}
+                >
+                  <ArtistSidePanel
+                    pic={artist.profilePic}
+                    name={artist.artistName}
+                    desc={artist.description}
+                    link={artist.url[0]}
+                  />
+                </Col>
+              )}
             </>
           )}
-
-          <Col className="col text-center mb-2 mb-lg-0 px-1">
-            {loader ? (
-              <Row>
-                <Col>
-                  <Spinner animation="grow" variant="dark" />
-                </Col>
-              </Row>
-            ) : (
-              <>
-                {error && <Alert variant="danger">{error}</Alert>}
-                {artist && (
-                  <Col
-                    xs={{ span: 2, order: 1 }}
-                    md={{ span: 3, order: 12 }}
-                    lg={{ span: 2, order: 12 }}
-                  >
-                    <ArtistSidePanel
-                      pic={artist.profilePic}
-                      name={artist.artistName}
-                      desc={artist.description}
-                      link={artist.url[0]}
-                    />
-                  </Col>
-                )}
-              </>
-            )}
-          </Col>
-        </Row>
-      </Container>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
